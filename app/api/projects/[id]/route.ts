b595e6cea483b1c2537990a61d3cba23a23b26ff
Hanyu-Lin <hanyulin02@gmail.com>
2023-03-07 10:45:32 +0100

    Todo

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,23 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const id = params.id;
-  const { projectName } = await request.json();
-  try {
-    const result = await prisma.projects.create({
-      data: {
-        name: projectName,
-      },
-    });
-    return NextResponse.json(result, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { err: "A error has occured when making a post" },
-      { status: 500 }
-    );
-  }
-}
+//CRUD endpoint for tasks in a specific project
