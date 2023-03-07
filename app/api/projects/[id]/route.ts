766428d279bd22977e3e7032833ffc429829dd3c
Hanyu-Lin <hanyulin02@gmail.com>
2023-03-07 13:23:36 +0100

    added checkValidProjectId method and end point for updating and deleting a task

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,5 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+async function checkValidProjectId(id: string) {
+  //check if project exists
+  const project = await prisma.projects.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  if (!project) {
+    return NextResponse.json({ err: "Wrong project id" }, { status: 400 });
+  }
+}
 
 //CRUD endpoint for tasks in a specific project
 export async function GET(
@@ -35,19 +46,9 @@ export async function POST(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  console.log("received");
   const { name } = await request.json();
   try {
-    //check if project exists
-    const project = await prisma.projects.findUnique({
-      where: {
-        id: params.id,
-      },
-    });
-    if (!project) {
-      return NextResponse.json({ err: "Wrong project id" }, { status: 400 });
-    }
-    console.log("project good");
+    checkValidProjectId(params.id);
     const task = await prisma.tasks.create({
       data: {
         name,
@@ -57,11 +58,51 @@ export async function POST(
         },
       },
     });
-    console.log(task);
     return NextResponse.json(task, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { err: "A error has occured when making a task" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id, completed } = await request.json();
+  try {
+    checkValidProjectId(params.id);
+    const task = await prisma.tasks.update({
+      where: { id },
+      data: {
+        completed,
+      },
+    });
+    return NextResponse.json(task, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { err: "A error has occured when updating the task" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await request.json();
+  try {
+    checkValidProjectId(params.id);
+    const task = await prisma.tasks.delete({
+      where: { id },
+    });
+    return NextResponse.json(task, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { err: "A error has occured when deleting the task" },
       { status: 500 }
     );
   }
