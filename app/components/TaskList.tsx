1cf0b7ae65fac65929a370421f8588743d87ff3c
Hanyu-Lin <hanyulin02@gmail.com>
2023-03-07 12:42:37 +0100

    using SWR to fetch the specific tasklist

--- app/components/TaskList.tsx
+++ app/components/TaskList.tsx
@@ -1,24 +1,40 @@
+"use client";
 import React from "react";
 import AddTaskInput from "./AddTaskInput";
 import TaskItem from "./TaskItem";
+import useSWR from "swr";
 import { Task } from "../../lib/types";
 interface TasksProps {
-  tasks: Task[];
+  projectId: string;
 }
 
-function Tasklist({ tasks }: TasksProps) {
+async function fetcher(input: RequestInfo): Promise<{ tasks: Task[] }> {
+  const res = await fetch(input);
+  return res.json();
+}
+
+function Tasklist({ projectId }: TasksProps) {
+  const { data, error, isLoading } = useSWR<{ tasks: Task[] }>(
+    `/api/projects/${projectId}`,
+    fetcher,
+    { refreshInterval: 1000 }
+  );
+
+  if (error) return error;
+  if (isLoading) return <div>Loading...</div>;
   return (
     <div>
-      <p>{`tasks: ${tasks ? tasks.length : 0}`}</p>
-      <AddTaskInput></AddTaskInput>
+      <p>{`tasks: ${data?.tasks ? data?.tasks.length : 0}`}</p>
+      <AddTaskInput projectId={projectId}></AddTaskInput>
       <ul>
-        {tasks &&
-          tasks.map((task) => (
+        {data?.tasks &&
+          data?.tasks.map((task) => (
             <TaskItem
               key={task.id}
               id={task.id}
               name={task.name}
               completed={task.completed}
+              projectId={task.projectId}
             ></TaskItem>
           ))}
       </ul>
