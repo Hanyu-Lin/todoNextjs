ea9d6016a7a2568892fc378a9eed1bdecbe3e828
Hanyu-Lin <hanyulin02@gmail.com>
2023-03-07 12:24:41 +0100

    added endpoint for GET request

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -2,3 +2,62 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
 //CRUD endpoint for tasks in a specific project
+export async function GET({ params }: { params: { id: string } }) {
+  try {
+    const project = await prisma.projects.findUnique({
+      where: {
+        id: params.id,
+      },
+      select: {
+        tasks: {
+          select: {
+            id: true,
+            name: true,
+            completed: true,
+            projectId: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json(project, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { err: "A error has occured when fetching the project" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { name } = await request.json();
+  console.log("received");
+  try {
+    //check if project exists
+    const project = await prisma.projects.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+    if (!project) {
+      return NextResponse.json({ err: "Wrong project id" }, { status: 400 });
+    }
+    const result = await prisma.tasks.create({
+      data: {
+        name,
+        completed: false,
+        project: {
+          connect: { id: params.id },
+        },
+      },
+    });
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { err: "A error has occured when making a task" },
+      { status: 500 }
+    );
+  }
+}
