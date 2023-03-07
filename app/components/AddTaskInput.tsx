7f0b4be1ab4728f4ca6a4e3c35de210aeac93830
Hanyu-Lin <hanyulin02@gmail.com>
2023-03-07 12:42:04 +0100

    Added create task functionality

---
+++ app/components/AddTaskInput.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { useState } from "react";
+interface InputProps {
+  projectId: string;
+}
+
+export const createTodo = async (projectId: string, name: string) => {
+  try {
+    const response = await fetch(`../api/projects/${projectId}`, {
+      method: "POST",
+      body: JSON.stringify({ name }),
+    });
+    console.log(response);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+function AddTaskInput({ projectId }: InputProps) {
+  const [name, setName] = useState("");
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        createTodo(projectId, name);
+        setName("");
+      }}
+    >
+      <input
+        placeholder="What to do..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+export default AddTaskInput;
